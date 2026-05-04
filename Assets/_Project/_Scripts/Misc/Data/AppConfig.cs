2eb542cfbd670af0d76da3a48feb41e3dc9b97d0
AlekseyShashkovJob <alexeyshashkov@syndicate-g.com>
2026-05-04 16:05:49 +0300

    feat : add easy launch

--- Assets/_Project/_Scripts/Misc/Data/AppConfig.cs
+++
@@ -1,7 +0,0 @@
-namespace Misc.Data
-{
-    public class AppConfig
-    {
-        public const string PRIVACY_URL = "https://juicypush.com/privacy-policy.html";
-    }
-}
--- Assets/_Project/_Scripts/Misc/Data/SceneConstants.cs
+++ Assets/_Project/_Scripts/Misc/Data/SceneConstants.cs
@@ -2,7 +2,6 @@ namespace Misc.Data
 {
     public class SceneConstants
     {
-        //public const string LOADER_SCENE = "0.SampleScene";
         public const string MENU_SCENE = "1.Menu";
         public const string GAME_SCENE = "2.Game";
     }
